8bc8f0e805bc0dc03b1059b32bd14af253ded701
Takahiro Iizuka <agdjgt@gmail.com>
2022-09-28 21:53:54 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <header>
+      <h1>My assignment of Vue Component!</h1>
+    </header>
+    <user-data @add-user="addUser"></user-data>
+    <active-user
+      v-for="user in users"
+      :key="user.username"
+      :name="user.username"
+      :age="user.age"
+    ></active-user>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          username: 'Daniel Jordan',
+          age: '27'
+        },
+        {
+          username: 'Ariel Jockson',
+          age: '33'
+        }
+      ]
+    };
+  },
+  methods: {
+    addUser(name, age) {
+      const newUser = {
+        username: name,
+        age: age
+      };
+      this.users.push(newUser);
+    }
+  }
+}
+</script>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Name: {{ name }}</h2>
+    <h3>Age: {{ age }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true
+    },
+    age: {
+      type: Number,
+      requiresd: true
+    }
+  },
+  data() {
+    return {
+      hello: 'This is component!'
+    }
+  }
+}
+</script>
